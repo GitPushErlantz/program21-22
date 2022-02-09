44e052e634367988f5e24f2eff9421576a949e8a
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-02-09 12:27:30 +0100

    a

---
+++ ud3/05-Erosketak/src/model/Bezeroa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author garate.erlantz
+ */
+public interface Bezeroa {
+    public int getKodea();
+    public String getIzena();
+    public String getHelbidea();
+    public String getEmaila();
+}
