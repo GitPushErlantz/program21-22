96fd345d63c8d56812e298318e186fb65c1b217c
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-02-18 14:11:26 +0100

    ProduktuenJFramea sortu

---
+++ ud3/05-Erosketak/src/model/ProduktuenTableModela.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author garate.erlantz
+ */
+public class ProduktuenTableModela extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        //ZERBAIT FALTA DA
+        return 7;
+    }
+
+    @Override
+    public int getColumnCount() {
+        //ZERBAIT FALTA DA
+        return 727;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        //ZERBAIT FALTA DA
+        return "WYSI";
+    }
+
+}
